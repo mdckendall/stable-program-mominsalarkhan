5d31f2f4c697da6916bb6e34bda8128237336215
Momin Salar <124016292+mominsalarkhan@users.noreply.github.com>
2023-02-28 04:20:03 +0000

    Stables changed

--- Stable.java
+++ Stable.java
@@ -1,1 +1,20 @@
-//Create the Stable class here.
+import java.util.ArrayList;//Create the Stable class here.
+
+public class Stable {
+  String address;
+  ArrayList<Horse> arrayList = new ArrayList<Horse>();
+
+  public Stable(String address) {
+    this.address = address;
+  }
+
+  public Stable() {
+  }
+
+  public void listHorses() {
+    for (int i = 0; i < arrayList.size(); i++) {
+      System.out.println("Horse #" + (i + 1) + " Name: " + arrayList.get(i).name + " Weight: "
+          + arrayList.get(i).weight + " Tame: " + arrayList.get(i).tame);
+    }
+  }
+}
